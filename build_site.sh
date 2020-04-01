d6620aed6b7dfc9431bb7c87fba43fe7f8e1494a
François Michonneau <francois.michonneau@gmail.com>
2020-04-01 09:18:15 -0400

    use custom entrypoint

---
+++ build_site.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -x # verbose mode
+set -e # stop executing after error
+
+echo "Starting Jekyll build"
+
+####################################################
+# Set workspace permissions
+####################################################
+
+chmod -R a+w /github/workspace
+
+####################################################
+# Build the Jekyll site
+####################################################
+
+gem install bundler
+bundle update
+bundle exec jekyll build --trace
+
+####################################################
+# Build completed
+####################################################
+
+echo "Completed Jekll build"
